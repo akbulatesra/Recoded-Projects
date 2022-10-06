bb9d9c8fe70c54bb86b47b5c440f86e9fab1c18b
esra akbulat <a.esra.akbulat@gmail.com>
2022-10-06 16:57:10 +0300

    Completed assignment

---
+++ js-projects/phase-1-arithmetic-lab-master/index.js
@@ -0,0 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 24;
+let num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(20, 1, 2, 3);
