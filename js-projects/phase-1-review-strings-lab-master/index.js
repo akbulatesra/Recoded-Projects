962f7e7aeefaea47b6437ad72fe39a492de6549e
esra akbulat <a.esra.akbulat@gmail.com>
2022-10-06 14:32:05 +0300

    Completed assignment

---
+++ js-projects/phase-1-review-strings-lab-master/index.js
@@ -0,0 +1,5 @@
+// Write your code in this file!
+let currentUser = "esra";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser[0]} !`;
