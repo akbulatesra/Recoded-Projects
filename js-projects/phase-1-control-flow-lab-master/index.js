cd9c1b3f86a982501015402bd145c5463192b6c4
esra akbulat <a.esra.akbulat@gmail.com>
2022-10-21 23:42:51 +0300

    Completed assignment"?"

---
+++ js-projects/phase-1-control-flow-lab-master/index.js
@@ -0,0 +1,35 @@
+function scuberGreetingForFeet(ride) {
+  // Write your code here!
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride > 400 && ride <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (ride > 2000 && ride <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(city) {
+  // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (true) {
+    case tip === "generous":
+      return "Thank you so much.";
+      break;
+    case tip !== "generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
+}
