0f6ad7ab739706a41cd4829bdc8c072d8ee757a8
esra akbulat <esra@klinikya.com>
2022-12-06 21:35:30 +0300

    "Completed assignment"

---
+++ js-projects/phase-0-pac-3-intro-to-functions-lab-master/index.js
@@ -0,0 +1,25 @@
+const shout = (string) => {
+  return string.toUpperCase();
+};
+
+const whisper = (string) => {
+  return string.toLowerCase();
+};
+
+const logShout = (string) => {
+  console.log(string.toUpperCase());
+};
+
+const logWhisper = (string) => {
+  console.log(string.toLowerCase());
+};
+
+const sayHiToHeadphonedRoommate = (string) => {
+  if (string === string.toLowerCase()) {
+    return `I can't hear you!`;
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  }
+};
